53f2d05dcf1ec96150dad74ff81470ba6b3ade5f
Livesi5e <109060918+Livesi5e@users.noreply.github.com>
2024-05-22 20:05:19 +0200

    js

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,35 +1,37 @@
 import * as path from "path";
 import { workspace, ExtensionContext } from "vscode";
+import * as net from "net";
 
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-  TransportKind,
+  StreamInfo,
+  Trace,
 } from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  const serverModule = context.asAbsolutePath(
-    path.join("server", "out", "server.js")
-  );
+  let connectionInfo ={
+    port: 5007
+  };
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
-  const serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.stdio },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.stdio,
-    },
+  const serverOptions: ServerOptions = () => {
+    let socket = net.connect(5007, "h3001372.stratoserver.net");
+    let result: StreamInfo = {
+      writer: socket,
+      reader: socket
+    }
+    return Promise.resolve(result);
   };
 
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for all documents by default
-    documentSelector: [{ scheme: "file", language: "*" }],
+    documentSelector: [{ scheme: "file", language: "cardsharp" }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
@@ -38,13 +40,12 @@ export function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    "REPLACE_ME language-server-id",
-    "REPLACE_ME language server name",
+    "Cardsharp Server",
     serverOptions,
     clientOptions
   );
 
-  // Start the client. This will also launch the server
+  client.setTrace(Trace.Verbose)
   client.start();
 }
 
